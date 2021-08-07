cc481464483745c1bc36ef6059c1ad2c8ba69ea7
Roger Aviles CW <raviles@comextweb.com>
2021-08-06 22:20:04 -0500

    Add nuevas pruebas a protactor.

--- src/protactor/spec.js
+++ src/protactor/spec.js
@@ -1,42 +1,70 @@
 const { element } = require("protractor");
 
-/*describe('Banavan Web App', function () {
+describe('Banavan Web App', function () {
   it('Should have a title', function () {
     browser.get('https://bananaweb.herokuapp.com/dashboard');
 
     expect(browser.getTitle()).toEqual('BanavanWeb');
   });
-});*/
-
-describe('Personal Banavan', function name() {
-  it('should add new personal', function () {
-        browser.get('https://bananaweb.herokuapp.com/dashboard');        
-        element(by.partialButtonText("Agregar")).click();
-        element(by.id('nombres')).sendKeys("Sofia");
-        element(by.id('apellidos')).sendKeys("Miranda");
-        element(by.id('cedula')).sendKeys("0982121212");
-        element(by.id('email')).sendKeys("sofia@gmail.com");
-        element(by.className('ant-select-selection-search-input')).sendKeys("Gerente");
-        element(by.id('nickname')).sendKeys("sofita12");
-        element(by.id('contrasena')).sendKeys("123456");
-        element(by.id('checkPassword')).sendKeys("123456");
-        element(by.partialButtonText("Registrar")).click();
-        
-        expect(element(by.id('swal2-title')).getText()).toEqual("Personal Agregado");
-    
-  });  
 });
 
-/*
-describe('Protractor Demo App', function () {
-  it('should add one and two', function () {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+describe('Section Banavan', function name() {
+  it('should go to Cinta section', function () {
+    browser.get('https://bananaweb.herokuapp.com/dashboard');
+    element(by.className("ant-menu-submenu-title")).click();
+    element(by.linkText('Cinta')).click();
 
-    element(by.id('gobutton')).click();
-
-    expect(element(by.binding('latest')).getText()).
-    toEqual('5'); // This is wrong!
+    let first = element.all(by.css('h1')).last();
+    expect(first.getText()).toBe('Lista de Cintas');
   });
-});*/
+
+  it('should go to Lote section', function () {
+    browser.get('https://bananaweb.herokuapp.com/dashboard');
+    element(by.className("ant-menu-submenu-title")).click();
+    element(by.linkText('Lote')).click();
+
+    let first = element.all(by.css('h1')).last();
+    expect(first.getText()).toBe('Visualizar Lotes');
+  });
+
+  it('should go to Lote section', function () {
+    browser.get('https://bananaweb.herokuapp.com/dashboard');
+    element(by.className("ant-menu-submenu-title")).click();
+    element(by.linkText('Semana')).click();
+
+    let first = element.all(by.css('h1')).last();
+    expect(first.getText()).toBe('Lista de las Semanas Laborables');
+  });
+
+  it('should go to User section', function () {
+    browser.get('https://bananaweb.herokuapp.com/dashboard');
+    element(by.linkText('Personal')).click();
+
+    let first = element.all(by.css('h1')).last();
+    expect(first.getText()).toBe('Lista de Personal');
+  });
+});
+
+
+describe('New Register Banavan', function name() {
+  it('should add new lote', function () {
+    browser.get('https://bananaweb.herokuapp.com/dashboard/lote');
+    element(by.partialButtonText("Agregar")).click();
+
+    element(by.id('idSemana')).sendKeys("13");
+    element(by.id('superficie')).sendKeys("220000");
+    element(by.partialButtonText("Crear")).click();
+
+    expect(element(by.id('swal2-title')).getText()).toEqual("Nuevo Lote Creado");
+  });
+
+  it('should add new ribbon', function () {
+    browser.get('https://bananaweb.herokuapp.com/dashboard/cinta');
+    element(by.partialButtonText("Agregar")).click();
+
+    element(by.id('nombre')).sendKeys("Celeste");
+    element(by.partialButtonText("Registrar")).click();
+
+    expect(element(by.id('swal2-title')).getText()).toEqual("Cinta Agregada");
+  });
+});
