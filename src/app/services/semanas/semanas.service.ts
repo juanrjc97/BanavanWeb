031895b611f2ae2bf6c4485d11a1af879a104120
Roger Aviles CW <raviles@comextweb.com>
2021-08-12 09:06:21 -0500

    Tabla Semanas muestra anio quemado.

--- src/app/services/semanas/semanas.service.ts
+++ src/app/services/semanas/semanas.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
 import { Semana } from 'src/app/models/semana';
 import { environment } from 'src/environments/environment';
 
@@ -12,7 +13,19 @@ export class SemanasService {
   constructor(private http: HttpClient) {}
 
   cargarSemanas() {
-    return this.http.get(this.get_semanas);
+    let paramsA = new HttpParams();
+    const anhos = ["2021"];
+    paramsA.append('anhos', JSON.stringify(anhos));
+
+    let options = {
+      /*headers: {
+        'Content-Type': 'application/json',
+      },*/
+      //params: { paramsA },
+      params: new HttpParams().append('anhos[]', '2021'),
+    };
+    //return this.http.get(this.get_semanas, {params: paramsA});
+    return this.http.get(this.get_semanas, options);
   }
 
   actualizarSemanas(Semana: any) {
