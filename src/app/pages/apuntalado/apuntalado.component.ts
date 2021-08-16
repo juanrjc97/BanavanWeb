50e0a74d128cd2a82df5ed608428c39af29465c1
juan jimenez <juanj121297@gmail.com>
2021-08-16 14:24:02 -0500

    pantalla de enfunde por semana

--- src/app/pages/apuntalado/apuntalado.component.ts
+++ src/app/pages/apuntalado/apuntalado.component.ts
@@ -1,6 +1,9 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-invalid-this */
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {InventarioService} from '../../services/inventario/inventario.service';
 
 @Component({
@@ -70,16 +73,22 @@ public data = {
 public titulos:any = [];
 public filas:any = [];
 public cargando = true;
-constructor(private inventarioService:InventarioService) { }
+public filterForm: FormGroup = this.fb.group({
+  semana: [null, [Validators.required, Validators.pattern('^[0-9]*$')]],
+  anho: [null, [Validators.required, Validators.pattern('^[0-9]*$')]],
+});
+constructor(private inventarioService:InventarioService, private fb: FormBuilder) {
+
+}
 
 ngOnInit(): void {
-  this.cargarInventario();
+
 }
 
 
-cargarInventario() {
+cargarInventario(idSemana: number, anho:number) {
   this.cargando =true;
-  this.inventarioService.cargarEnfundado(4, 2019).subscribe(
+  this.inventarioService.cargarEnfundado(idSemana, anho).subscribe(
       (resp:any)=>{
         console.log(resp);
         this.formatearResp(resp);
@@ -93,5 +102,19 @@ formatearResp(resp: any) {
   Object['values'](resp.dataset).forEach((element:any) => {
     this.filas.push(Object['values'](element));
   });
+}
+
+submitForm() {
+  if (this.filterForm.invalid) {
+    for (const i in this.filterForm.controls) {
+      this.filterForm.controls[i].markAsDirty();
+      this.filterForm.controls[i].updateValueAndValidity();
+    }
+    return;
+  }
+
+  this.cargarInventario(this.filterForm.get('semana')?.value,
+      this.filterForm.get('anho')?.value );
+  console.log(this.filterForm.value);
 }
 }
--- src/app/pages/lote/lote.component.ts
+++ src/app/pages/lote/lote.component.ts
@@ -1,114 +1,108 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Lote } from 'src/app/models/lote';
+/* eslint-disable require-jsdoc */
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Lote} from 'src/app/models/lote';
 import Swal from 'sweetalert2';
-import { LoteService } from '../../services/lote/lote.service';
+import {LoteService} from '../../services/lote/lote.service';
 
 
 @Component({
   selector: 'app-lote',
   templateUrl: './lote.component.html',
-  styleUrls: ['./lote.component.css']
+  styleUrls: ['./lote.component.css'],
 })
 export class LoteComponent implements OnInit {
-
   public editCache: { [key: number]: { edit: boolean; data: Lote } } = {};
   public listOfData: Lote[] = [];
-  public  size = 8;
-  public  isVisible = false;
+  public size = 8;
+  public isVisible = false;
 
   public loteForm:FormGroup = this.fb.group({
-    numero: [null, [Validators.required, Validators.pattern("^[0-9]*$"),]  ],
-    superficie: [null, [Validators.required, Validators.pattern("^[0-9]*$"),]]
+    numero: [null, [Validators.required, Validators.pattern('^[0-9]*$')]],
+    superficie: [null, [Validators.required, Validators.pattern('^[0-9]*$')]],
   });
 
   constructor(private fb: FormBuilder, private loteService: LoteService) { }
 
   ngOnInit(): void {
-      this.cargarLotes();
+    this.cargarLotes();
   }
 
-  cargarLotes(){
-    this.loteService.cargarLotes().subscribe( 
-      (resp:any) =>  {
-       
-        this.listOfData = resp.lotes;
-        console.log(this.listOfData);
-        this.updateEditCache();
-      }
-    )
+  cargarLotes() {
+    this.loteService.cargarLotes().subscribe(
+        (resp:any) => {
+          this.listOfData = resp.lotes;
+          console.log(this.listOfData);
+          this.updateEditCache();
+        },
+    );
   }
 
-  //hacer aqui la llamada de post 
-  crearLote( ){
+  // hacer aqui la llamada de post
+  crearLote( ) {
     const lote = {
-     ...this.loteForm.value
-    }
+      ...this.loteForm.value,
+    };
     console.log(lote);
     this.loteService.crearLote(lote).subscribe(
-      (resp:any)=>{
-        Swal.fire('Nuevo Lote Creado', `El lote  se ha creado co Exito`, 'success')
-        this.cargarLotes();
-      },(err)=>{
-        Swal.fire('Error','Ocurrio un erro al crear el Lote','error')
-        console.log(err);
-      }
-    ) 
-
+        (resp:any)=>{
+          Swal.fire('Nuevo Lote Creado', `El lote  se ha creado co Exito`, 'success');
+          this.cargarLotes();
+        }, (err)=>{
+          Swal.fire('Error', 'Ocurrio un erro al crear el Lote', 'error');
+          console.log(err);
+        },
+    );
   }
 
 
-startEdit(id: number): void {
-  this.editCache[id].edit = true;
-}
-
-saveEdit(id: number): void {  
-  const index = this.listOfData.findIndex(item => item.id === id);
-  Object.assign(this.listOfData[index], this.editCache[id].data);
-  this.editCache[id].edit = false;
-  console.log("lote compo");
-  this.loteService.actualizarLote( this.editCache[id].data).subscribe(
-    (resp:any)=>{
-      Swal.fire('Lote Actualizado','Se ha actualizado exitosamente', 'success');
-      console.log('Lote actualizo');
-      
-    }, (err)=>{
-      Swal.fire('Error','Sucedio un error, no se pudo actualizar el elemento', 'error');
-      this.editCache[id].edit = false;
-      console.log("Error al actualizat el Lote" + id);
-    }
-  )
-
-}
-
-deleteRow(id: number): void {
-  
-  this.loteService.eliminarLote(id).subscribe(
-    (resp:any)=>{
-      Swal.fire('Exito',"Elemento eliminado correctamente",'success')
-      console.log("Lote eliminado");
-      this.listOfData = this.listOfData.filter(d => d.id !== id);
-    },(err)=>{
-      Swal.fire('Error',"Ocurrio un error al eliminar el elemento",'error')
-      console.log("Error al elminar el Lote" + id + "\n" +err);
-    }
-  );
-  
-}
-
-updateEditCache(): void {
-  this.listOfData.forEach(
-    item => {
-    this.editCache[item.id] = {
-      edit: false,
-      data: { ...item } // con los 3 puntos se genera un copia completa del objeto al cual se le
-                        // esta iterando (crea otro elemento igual) es como una copia sin referencia 
-                        //al objeto anterior 
-    };
+  startEdit(id: number): void {
+    this.editCache[id].edit = true;
   }
-  );
- // console.log(this.editCache);
-}
+
+  saveEdit(id: number): void {
+    const index = this.listOfData.findIndex((item) => item.id === id);
+    Object.assign(this.listOfData[index], this.editCache[id].data);
+    this.editCache[id].edit = false;
+    console.log('lote compo');
+    this.loteService.actualizarLote( this.editCache[id].data).subscribe(
+        (resp:any)=>{
+          Swal.fire('Lote Actualizado', 'Se ha actualizado exitosamente', 'success');
+          console.log('Lote actualizo');
+        }, (err)=>{
+          Swal.fire('Error', 'Sucedio un error, no se pudo actualizar el elemento', 'error');
+          this.editCache[id].edit = false;
+          console.log('Error al actualizat el Lote' + id);
+        },
+    );
+  }
+
+  deleteRow(id: number): void {
+    this.loteService.eliminarLote(id).subscribe(
+        (resp:any)=>{
+          Swal.fire('Exito', 'Elemento eliminado correctamente', 'success');
+          console.log('Lote eliminado');
+          this.listOfData = this.listOfData.filter((d) => d.id !== id);
+        }, (err)=>{
+          Swal.fire('Error', 'Ocurrio un error al eliminar el elemento', 'error');
+          console.log('Error al elminar el Lote' + id + '\n' +err);
+        },
+    );
+  }
+
+  updateEditCache(): void {
+    this.listOfData.forEach(
+        (item) => {
+          this.editCache[item.id] = {
+            edit: false,
+            data: {...item}, // con los 3 puntos se genera un copia completa del objeto al cual se le
+            // esta iterando (crea otro elemento igual) es como una copia sin referencia
+            // al objeto anterior
+          };
+        },
+    );
+    // console.log(this.editCache);
+  }
 
   showModal(): void {
     this.isVisible = true;
@@ -116,10 +110,10 @@ updateEditCache(): void {
 
   handleCancel(): void {
     this.isVisible = false;
-    this.loteForm.reset();   
+    this.loteForm.reset();
   }
 
-  //hacer aqui la llamada a crearLote
+  // hacer aqui la llamada a crearLote
   submitForm(): void {
     if (this.loteForm.invalid) {
       for (const i in this.loteForm.controls) {
@@ -130,6 +124,6 @@ updateEditCache(): void {
     }
     this.crearLote();
     this.isVisible = false;
-    this.loteForm.reset();    
+    this.loteForm.reset();
   }
 }
